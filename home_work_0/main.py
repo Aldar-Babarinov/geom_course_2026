699dc84e6e6dcab6ebd0815ccfe6ea8ec8710cea
Babarinov Aldar <babarinovaldar@gmail.com>
2026-09-15 04:01:53 +0300

    выполнил home_work_0: проверил версии библиотек

---
+++ home_work_0/main.py
@@ -0,0 +1,8 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+print(f"Версия Python: {sys.version.split()}")
+print(f"Версия NumPy: {np.__version__}")
+print(f"Версия Pandas: {pd.__version__}")
+print(f"Версия Matplotlib: {matplotlib.__version__}")
